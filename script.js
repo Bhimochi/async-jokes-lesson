ad2a5de56f0af182814434c9361df816b4198eb6
Berni Wong <3colouredmochi@gmail.com>
2022-12-05 21:41:06 +1100

    math done

--- script.js
+++ script.js
@@ -1,21 +1,4 @@
-function getJoke(cb) {
-    req = new XMLHttpRequest()
-    req.addEventListener('load', event => cb(event.target.response.joke))
-    req.open('GET', 'https://icanhazdadjoke.com/')
-    req.setRequestHeader('Accept', 'application/json')
-    req.responseType = 'json'
-    req.send()
-}
+import {add, PI} from './math.js'
 
-const jokes = []
-
-getJoke(joke => {
-    jokes.push(joke)
-    getJoke(joke => {
-        jokes.push(joke)
-        getJoke(joke => {
-            jokes.push(joke)
-            console.log(jokes)
-        })
-    })
-})
+// console.log(foo)
+console. log(add(5, 7))
